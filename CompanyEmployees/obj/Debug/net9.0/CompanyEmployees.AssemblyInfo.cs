ef630b2934e5fa99b71b06f5d3b5b1e035b47358
mahmoud-elboraiy <mahmoudelboraiy25@gmail.com>
2025-06-16 12:47:25 +0300

    delete weather controller

--- CompanyEmployees/obj/Debug/net9.0/CompanyEmployees.AssemblyInfo.cs
+++ CompanyEmployees/obj/Debug/net9.0/CompanyEmployees.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a157911721d67209dae679338cdaf4f949a70ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CompanyEmployees")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
