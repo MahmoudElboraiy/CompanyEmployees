fbc5085f478265d88c5ff5ff39883cf87f50aac9
mahmoud-elboraiy <mahmoudelboraiy25@gmail.com>
2025-06-18 18:08:04 +0300

    ConfigureIISIntegration

--- CompanyEmployees/Extensions/ServiceExtensions.cs
+++ CompanyEmployees/Extensions/ServiceExtensions.cs
@@ -1,6 +1,23 @@
 ﻿namespace CompanyEmployees.Extensions
 {
-    public class ServiceExtensions
+    public static class ServiceExtensions
     {
+        /*
+         * CORS (Cross-Origin Resource Sharing) is a mechanism to give or restrict
+            access rights to applications from different domains.
+         */
+        public static void ConfigureCors(this IServiceCollection services) =>
+         services.AddCors(options =>
+         {
+             options.AddPolicy("CorsPolicy", builder =>
+             builder.AllowAnyOrigin()
+             .AllowAnyMethod()
+             .AllowAnyHeader());
+         });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+         services.Configure<IISOptions>(options =>
+         {
+         });
     }
 }
